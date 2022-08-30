f324f31c2730aa851ba9d1316cb8b48343a322a3
tren <tren@parasoft.com>
2022-08-30 10:55:31 +0800

    @task PDA-998 Implement UI for gear button and open the window

---
+++ app/src/main/java/com/parasoft/demoapp/Component/SettingDialog.java
@@ -0,0 +1,55 @@
+package com.parasoft.demoapp.Component;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.parasoft.demoapp.LoginActivity;
+import com.parasoft.demoapp.R;
+
+public class SettingDialog extends DialogFragment {
+
+    public static String TAG = "SettingDialog";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.setting_dialog_layout, container, false);
+        setClickEvent(view);
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        Window window = getDialog().getWindow();
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        window.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        WindowManager.LayoutParams params = window.getAttributes();
+        params.width = (int) (displayMetrics.widthPixels * 0.9);
+        window.setAttributes((WindowManager.LayoutParams) params);
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        super.onStart();
+    }
+
+    public void setClickEvent(View view) {
+        view.findViewById(R.id.dismiss_button).setOnClickListener(v -> dismiss());
+
+        view.findViewById(R.id.save_button).setOnClickListener(v -> {
+            LoginActivity loginActivity = (LoginActivity) getActivity();
+            Toast.makeText(loginActivity, "TODO", Toast.LENGTH_SHORT).show();
+        });
+    }
+}
--- app/src/main/java/com/parasoft/demoapp/LoginActivity.java
+++ app/src/main/java/com/parasoft/demoapp/LoginActivity.java
@@ -2,8 +2,16 @@ package com.parasoft.demoapp;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.parasoft.demoapp.Component.SettingDialog;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -12,12 +20,29 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        ActionBar actionBar = getSupportActionBar();
-        if(actionBar != null){
-            actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-            actionBar.setCustomView(R.layout.title_layout);
-        }
+        initCustomActionBar();
     }
 
+    public void initCustomActionBar() {
+        ActionBar.LayoutParams lp = new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT, Gravity.CENTER);
+        View customActionBar = LayoutInflater.from(this).inflate(R.layout.title_layout, null);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar == null){
+            return;
+        }
+
+        actionBar.setCustomView(customActionBar, lp);
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+
+        Toolbar parent = (Toolbar) customActionBar.getParent();
+        parent.setContentInsetsAbsolute(0, 0);
+
+        ImageButton settingButton = (ImageButton) customActionBar.findViewById(R.id.settingButton);
+        settingButton.setOnClickListener(view -> openSettingModal());
+    }
+
+    public void openSettingModal() {
+        SettingDialog dialog = new SettingDialog();
+        dialog.show(getSupportFragmentManager(), SettingDialog.TAG);
+    }
 }
