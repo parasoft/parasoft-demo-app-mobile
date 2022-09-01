ec09d78a2d23c748649a6f252e05ec7b24f50a31
dtang <dtang@parasoft.com>
2022-09-01 10:08:18 +0800

    @task PDA-999 Save base URL

---
+++ app/src/main/java/com/parasoft/demoapp/util/SettingsUtil.java
@@ -0,0 +1,18 @@
+package com.parasoft.demoapp.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SettingsUtil {
+
+    public static void saveSetting(Context context, String name, String value) {
+        SharedPreferences.Editor note = context.getSharedPreferences("applicationSettings", Context.MODE_PRIVATE).edit();
+        note.putString(name, value);
+        note.apply();
+    }
+
+    public static String getSetting(Context context, String name) {
+        SharedPreferences read = context.getSharedPreferences("applicationSettings", Context.MODE_PRIVATE);
+        return read.getString(name, null);
+    }
+}
