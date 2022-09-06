3c17b66ed8f6c89f8a7a8ab2bfff2f65d39f6dc4
rliu-parasoft <rliu@parasoft.com>
2022-09-06 18:43:04 +0800

    @task PDA-1001 Optimized code

--- app/src/main/java/com/parasoft/demoapp/component/UserInformationDialog.java
+++ app/src/main/java/com/parasoft/demoapp/component/UserInformationDialog.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TableLayout;
 import android.widget.TextView;
@@ -26,69 +27,82 @@ import com.parasoft.demoapp.retrofitConfig.response.ForgotPasswordUserInfo;
 import com.parasoft.demoapp.retrofitConfig.response.ResultResponse;
 
 import java.util.List;
+import java.util.Objects;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class UserInformationDialog extends DialogFragment {
-    public static String TAG = "UserInformationDialog";
+    public static final String TAG = "UserInformationDialog";
     public static final int LOADING = 1;
-    public static final int LOADERROR = 2;
+    public static final int LOAD_ERROR = 2;
     public static final int LOADED = 3;
-    public static final int NODATA = 4;
+    public static final int NO_DATA = 4;
+
     private ProgressBar progressBar;
     private TableLayout userInformationTable;
     private TextView textDisplayArea;
     private TextView usernameMessage;
     private TextView passwordMessage;
+    private Button closeButton;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.user_information_dialog_layout, container, false);
-        setClickEvent(view);
 
         progressBar = view.findViewById(R.id.progressBar);
         userInformationTable = view.findViewById(R.id.user_information_table);
         textDisplayArea = view.findViewById(R.id.textDisplayArea);
         usernameMessage = view.findViewById(R.id.username_value);
         passwordMessage = view.findViewById(R.id.password_value);
+        closeButton = view.findViewById(R.id.close_button);
 
         loadContent(LOADING, null);
+        closeButton.setOnClickListener(v -> dismiss());
 
         try {
             PDAService.getClient(ApiInterface.class).forgotPassword()
-                    .enqueue(new Callback<ResultResponse<List<ForgotPasswordUserInfo>>>() {
-                        @Override
-                        public void onResponse(Call<ResultResponse<List<ForgotPasswordUserInfo>>> call, Response<ResultResponse<List<ForgotPasswordUserInfo>>> response) {
-                            if(response.isSuccessful()) {
-                                ResultResponse<List<ForgotPasswordUserInfo>> result = response.body();
-                                if (result == null) return;
-                                List<ForgotPasswordUserInfo> userInfoList = result.getData();
-                                if (userInfoList == null) return;
-                                for (ForgotPasswordUserInfo userInfo: userInfoList) {
-                                    if (userInfo.getRoleName().equals("ROLE_APPROVER")) {
-                                        if (userInfo.isHasPrimaryUser()) {
-                                            loadContent(LOADED, userInfo);
-                                        } else {
-                                            loadContent(NODATA, null);
-                                        }
-                                    }
-                                }
-                                return;
-                            }
+                .enqueue(new Callback<ResultResponse<List<ForgotPasswordUserInfo>>>() {
+                    @Override
+                    public void onResponse(@NonNull Call<ResultResponse<List<ForgotPasswordUserInfo>>> call,
+                                           @NonNull Response<ResultResponse<List<ForgotPasswordUserInfo>>> response) {
+                        if (response.code() != 200) {
+                            loadContent(LOAD_ERROR, null);
+                            return;
                         }
 
-                        @Override
-                        public void onFailure(Call<ResultResponse<List<ForgotPasswordUserInfo>>> call, Throwable t) {
-                            Log.e("UserInformationDialog", "Forgot password request error", t);
-                            loadContent(LOADERROR, null);
+                        ResultResponse<List<ForgotPasswordUserInfo>> resultResponse = response.body();
+                        if (resultResponse == null || resultResponse.getData() == null) {
+                            loadContent(NO_DATA, null);
+                            return;
                         }
-                    });
+
+                        List<ForgotPasswordUserInfo> userInfoList = resultResponse.getData();
+                        ForgotPasswordUserInfo approverUserInfo = null;
+                        for (ForgotPasswordUserInfo userInfo: userInfoList) {
+                            if ("ROLE_APPROVER".equals(userInfo.getRoleName())) {
+                                approverUserInfo = userInfo;
+                                break;
+                            }
+                        }
+                        if(approverUserInfo == null || !approverUserInfo.isHasPrimaryUser()) {
+                            loadContent(NO_DATA, null);
+                        } else {
+                            loadContent(LOADED, approverUserInfo);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull Call<ResultResponse<List<ForgotPasswordUserInfo>>> call, @NonNull Throwable t) {
+                        Log.e(TAG, "Forgot password request error", t);
+                        loadContent(LOAD_ERROR, null);
+                    }
+                });
         } catch (IllegalArgumentException e) {
-            Log.e("UserInformationDialog", "Base URL error", e);
-            loadContent(LOADERROR, null);
+            Log.e(TAG, "Base URL error", e);
+            loadContent(LOAD_ERROR, null);
         }
         return view;
     }
@@ -104,44 +118,38 @@ public class UserInformationDialog extends DialogFragment {
         window.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         WindowManager.LayoutParams params = window.getAttributes();
         params.width = (int) (displayMetrics.widthPixels * 0.9);
-        window.setAttributes((WindowManager.LayoutParams) params);
+        window.setAttributes(params);
         window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
         super.onStart();
     }
 
-    private void setClickEvent(View view) {
-        view.findViewById(R.id.close_button).setOnClickListener(v -> dismiss());
-    }
-
-    private void loadContent(int loading, ForgotPasswordUserInfo userInfo) {
-        switch (loading) {
-            case 1: // when loading
-                progressBar.setVisibility(View.VISIBLE);
-                userInformationTable.setVisibility(View.INVISIBLE);
-                textDisplayArea.setVisibility(View.INVISIBLE);
-                break;
-            case 2: // when failed to load
-                if (isAdded()) {
+    private void loadContent(int loadStatus, @Nullable ForgotPasswordUserInfo userInfo) {
+        if(isAdded()) {
+            switch (loadStatus) {
+                case LOADING: // when loading
+                    progressBar.setVisibility(View.VISIBLE);
+                    userInformationTable.setVisibility(View.INVISIBLE);
+                    textDisplayArea.setVisibility(View.INVISIBLE);
+                    break;
+                case LOAD_ERROR: // when failed to load
                     textDisplayArea.setText(getResources().getString(R.string.wrong_base_url));
                     textDisplayArea.setTextColor(getResources().getColor(R.color.error));
                     progressBar.setVisibility(View.INVISIBLE);
                     textDisplayArea.setVisibility(View.VISIBLE);
-                }
-                break;
-            case 3: // when load completed with data
-                usernameMessage.setText(userInfo.getPrimaryUserInfo().getUserName());
-                passwordMessage.setText(userInfo.getPrimaryUserInfo().getPassword());
-                progressBar.setVisibility(View.INVISIBLE);
-                userInformationTable.setVisibility(View.VISIBLE);
-                break;
-            case 4: // when load completed but with no data
-                if (isAdded()) {
+                    break;
+                case LOADED: // when load completed with data
+                    usernameMessage.setText(Objects.requireNonNull(userInfo).getPrimaryUserInfo().getUserName());
+                    passwordMessage.setText(userInfo.getPrimaryUserInfo().getPassword());
+                    progressBar.setVisibility(View.INVISIBLE);
+                    userInformationTable.setVisibility(View.VISIBLE);
+                    break;
+                case NO_DATA: // when load completed but with no data
                     textDisplayArea.setText(getResources().getString(R.string.no_user_with_this_role));
                     textDisplayArea.setTextColor(getResources().getColor(R.color.dark_blue));
                     progressBar.setVisibility(View.INVISIBLE);
-                    userInformationTable.setVisibility(View.VISIBLE);
-                }
+                    textDisplayArea.setVisibility(View.VISIBLE);
+            }
         }
     }
 }
