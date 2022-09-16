1cb548f4530e94033c2133bd361a2b1d1ba5dd2b
dtang <dtang@parasoft.com>
2022-09-16 10:53:25 +0800

    @task PDA-1077 Display first half page of the dialog

--- app/src/main/java/com/parasoft/demoapp/component/OrderDialog.java
+++ app/src/main/java/com/parasoft/demoapp/component/OrderDialog.java
@@ -3,6 +3,7 @@ package com.parasoft.demoapp.component;
 import android.app.Dialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -14,18 +15,25 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.fragment.app.DialogFragment;
 
+import com.parasoft.demoapp.HomeActivity;
 import com.parasoft.demoapp.R;
 import com.parasoft.demoapp.retrofitConfig.ApiInterface;
 import com.parasoft.demoapp.retrofitConfig.PDAService;
 import com.parasoft.demoapp.retrofitConfig.request.OrderStatusRequest;
 import com.parasoft.demoapp.retrofitConfig.response.OrderResponse;
 import com.parasoft.demoapp.retrofitConfig.response.ResultResponse;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -41,6 +49,17 @@ public class OrderDialog extends DialogFragment {
     private TextView errorMessage;
     private OrderResponse orderInfo;
     private PDAService pdaService;
+    private LinearLayout comments;
+    private TextView commentsDetail;
+    private TextView orderStatus;
+    private TextView orderTimeYear;
+    private TextView orderTimeHour;
+    private TextView purchaserName;
+    private TextView location;
+    private TextView receiverName;
+    private TextView gpsCoordinates;
+    private ImageView map;
+    private HomeActivity homeActivity;
 
     public OrderDialog(String orderNumber) {
         this.orderNumber = orderNumber;
@@ -55,6 +74,17 @@ public class OrderDialog extends DialogFragment {
         cancelButton = view.findViewById(R.id.order_dismiss_button);
         closeButton = view.findViewById(R.id.order_close_button);
         errorMessage = view.findViewById(R.id.order_info_error_message );
+        comments = view.findViewById(R.id.comments);
+        commentsDetail = view.findViewById(R.id.comments_detail);
+        orderStatus = view.findViewById(R.id.order_status);
+        orderTimeYear = view.findViewById(R.id.order_time_year);
+        orderTimeHour = view.findViewById(R.id.order_time_hour);
+        purchaserName = view.findViewById(R.id.purchaser_name);
+        location = view.findViewById(R.id.location);
+        receiverName = view.findViewById(R.id.receiver_name);
+        gpsCoordinates = view.findViewById(R.id.gps_coordinates);
+        map = view.findViewById(R.id.map);
+        homeActivity = (HomeActivity) getActivity();
         setClickEvent();
         TextView orderDialogTitle = view.findViewById(R.id.order_dialog_title);
         orderDialogTitle.setText(getString(R.string.order_dialog_title, orderNumber));
@@ -98,6 +128,7 @@ public class OrderDialog extends DialogFragment {
                     int code = response.code();
                     if(code == 200) {
                         orderInfo = response.body().getData();
+                        setOrderLayout();
                         if (!orderInfo.getReviewedByAPV()) {
                             updateOrderStatus(orderInfo);
                         }
@@ -136,5 +167,108 @@ public class OrderDialog extends DialogFragment {
                         Log.e("OrderDialog", "Update Order status failed", t);
                     }
                 });
+    }
+
+    public void setOrderLayout() {
+        if (orderInfo.getComments() == null || orderInfo.getComments().length() == 0) {
+            comments.setVisibility(View.GONE);
+        } else {
+            comments.setVisibility(View.VISIBLE);
+            commentsDetail.setText(orderInfo.getComments());
+            if(commentsDetail.getLineCount() == 1) {
+                commentsDetail.setGravity(Gravity.END);
+            } else {
+                commentsDetail.setGravity(Gravity.START);
+            }
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        String date = sdf.format(orderInfo.getSubmissionDate());
+        orderTimeYear.setText(date.substring(0, 10));
+        orderTimeHour.setText(date.substring(11));
+        orderStatus.setText(getStatus(orderInfo.getStatus()));
+        purchaserName.setText(orderInfo.getRequestedBy());
+        location.setText(getRegion(orderInfo.getRegion()));
+        receiverName.setText(orderInfo.getReceiverId());
+        gpsCoordinates.setText(orderInfo.getLocation());
+        map.setImageDrawable(getMap(orderInfo.getOrderImage()));
+    }
+
+    public String getRegion(String region) {
+        switch (region) {
+            case "LOCATION_1" :
+                region = getResources().getString(R.string.location_1);
+                break;
+            case "LOCATION_2" :
+                region = getResources().getString(R.string.location_2);
+                break;
+            case "LOCATION_3" :
+                region = getResources().getString(R.string.location_3);
+                break;
+            case "LOCATION_4" :
+                region = getResources().getString(R.string.location_4);
+                break;
+            case "LOCATION_5" :
+                region = getResources().getString(R.string.location_5);
+                break;
+            case "LOCATION_6" :
+                region = getResources().getString(R.string.location_6);
+                break;
+            case "LOCATION_7" :
+                region = getResources().getString(R.string.location_7);
+                break;
+            case "LOCATION_8" :
+                region = getResources().getString(R.string.location_8);
+                break;
+        }
+        return region;
+    }
+
+    public String getStatus(String status) {
+        switch (status) {
+            case "SUBMITTED":
+                status = getResources().getString(R.string.status_open);
+                break;
+            case "DECLINED":
+                status = getResources().getString(R.string.status_denied);
+                orderStatus.setTextColor(getResources().getColor(R.color.light_black));
+                break;
+            case "APPROVED":
+                status = getResources().getString(R.string.status_approved);
+                orderStatus.setTextColor(getResources().getColor(R.color.light_black));
+                break;
+        }
+        return status;
+    }
+
+    public Drawable getMap(String image) {
+        Drawable drawable = null;
+        image = image.substring(image.lastIndexOf("-") + 1);
+        switch (image) {
+            case "1.png":
+                drawable = AppCompatResources.getDrawable( homeActivity, R.drawable.generic_map_1);
+                break;
+            case "2.png":
+                drawable = AppCompatResources.getDrawable( homeActivity, R.drawable.generic_map_2);
+                break;
+            case "3.png":
+                drawable = AppCompatResources.getDrawable( homeActivity, R.drawable.generic_map_3);
+                break;
+            case "4.png":
+                drawable = AppCompatResources.getDrawable( homeActivity, R.drawable.generic_map_4);
+                break;
+            case "5.png":
+                drawable = AppCompatResources.getDrawable( homeActivity, R.drawable.generic_map_5);
+                break;
+            case "6.png":
+                drawable = AppCompatResources.getDrawable( homeActivity, R.drawable.generic_map_6);
+                break;
+            case "7.png":
+                drawable = AppCompatResources.getDrawable( homeActivity, R.drawable.generic_map_7);
+                break;
+            case "8.png":
+                drawable = AppCompatResources.getDrawable( homeActivity, R.drawable.generic_map_8);
+                break;
+        }
+        return drawable;
     }
 }
