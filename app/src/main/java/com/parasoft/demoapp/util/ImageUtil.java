1afb412d7d5e439366eb33af4d74fec9791f846b
yshi <yshi@parasoft.com>
2022-09-27 11:12:10 -0700

    Refactor the code to reduce redundancy

--- app/src/main/java/com/parasoft/demoapp/util/ImageUtil.java
+++ app/src/main/java/com/parasoft/demoapp/util/ImageUtil.java
@@ -1,12 +1,10 @@
 package com.parasoft.demoapp.util;
 
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import com.parasoft.demoapp.R;
 import androidx.annotation.NonNull;
@@ -24,50 +22,40 @@ public class ImageUtil {
 
     public static void loadImage(ImageView imageView, String orderImage, @Nullable OrderItemAdapter.ViewHolder viewHolder) {
         PDAService pdaService = new PDAService();
-        if (viewHolder != null) {
-            editItemLayout(120, View.GONE, imageView, viewHolder);
-        }
+        showImage(false, null, imageView, viewHolder);
         pdaService.getClient(ApiInterface.class).getImage(orderImage)
                 .enqueue(new Callback<ResponseBody>() {
                     @Override
                     public void onResponse(@NonNull Call<ResponseBody> call, @NonNull Response<ResponseBody> response) {
                         if (response.code() == 200) {
-                            if (viewHolder != null) {
-                                editItemLayout(5, View.VISIBLE, imageView, viewHolder);
-                            }
                             assert response.body() != null;
-                            Bitmap bitmap = BitmapFactory.decodeStream(response.body().byteStream());
-                            imageView.setImageBitmap(bitmap);
+                            Bitmap image = BitmapFactory.decodeStream(response.body().byteStream());
+                            showImage(true, image, imageView, viewHolder);
                         } else {
-                            showLoadingFailedImage(imageView, viewHolder);
+                            showFailedImage(imageView);
                         }
                     }
 
                     @Override
                     public void onFailure(@NonNull Call<ResponseBody> call, @NonNull Throwable t) {
-                        showLoadingFailedImage(imageView, viewHolder);
+                        showFailedImage(imageView);
                         Log.e("OrderDialog", "Load image " + orderImage + " failed", t);
                     }
                 });
     }
 
-    private static void showLoadingFailedImage (ImageView imageView, @Nullable OrderItemAdapter.ViewHolder viewHolder) {
-        imageView.getLayoutParams().height = dp2px(100);
-        imageView.getLayoutParams().width = dp2px(65);
+    private static void showFailedImage(ImageView imageView) {
+        imageView.setScaleType(ImageView.ScaleType.CENTER);
         imageView.setImageResource(R.mipmap.ic_image_loading_failed);
+    }
+
+    private static void showImage(boolean show, Bitmap image, ImageView imageView, @Nullable OrderItemAdapter.ViewHolder viewHolder) {
+        if (show && image != null) {
+            imageView.setImageBitmap(image);
+        }
         if (viewHolder != null) {
-            editItemLayout(170, View.GONE, imageView, viewHolder);
+            viewHolder.darkOverlay.setVisibility(show ? View.VISIBLE : View.INVISIBLE);
         }
     }
 
-    private static void editItemLayout (int leftMargin, int visibility, ImageView imageView, @Nullable OrderItemAdapter.ViewHolder viewHolder) {
-        viewHolder.darkOverlay.setVisibility(visibility);
-        imageView.getLayoutParams().height = dp2px(65);
-        LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) viewHolder.orderItemNameLayout.getLayoutParams();
-        params.leftMargin = dp2px(leftMargin);
-    }
-
-    private static int dp2px(int dp) {
-        return (int) (dp * Resources.getSystem().getDisplayMetrics().density + 0.5);
-    }
 }
